8319377523c54ec95f94be4e03e50554b8d26968
manojpramesh <manojpramesh@gmail.com>
2017-05-09 21:22:21 +0530

    Display recent blocks

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,32 @@
-/* globals require, module */
+/* globals require, module, global */
 
 var express = require('express');
 var router = express.Router();
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider(global.web3Provider));
+
 
 router.get('/home', function (req, res) {
   res.locals = {
-      title: "Eth Panel"
+    title: "Eth Panel",
+    blocks: getlatestBlocks(10)
   };
   res.render('index');
 });
 
-router.get('/doc', function(req, res) {
+router.get('/doc', function (req, res) {
   res.render('../doc/index');
 });
 
+
+var getlatestBlocks = function (n) {
+  var result = [];
+  var lastBlock = web3.eth.blockNumber;
+  for (var i = lastBlock; i > lastBlock - n; i--) {
+    result.push(web3.eth.getBlock(i));
+  }
+  return result;
+}
 
 module.exports = router;
